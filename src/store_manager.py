baa55eb2fecce67ecd4e74eb7d625183155c6208
Pascal Bourgoin <pascal680@hotmail.ca>
2026-02-19 14:50:18 -0500

    Completions tous les etapes

--- src/store_manager.py
+++ src/store_manager.py
@@ -12,8 +12,20 @@ from orders.controllers.user_controller import create_user, remove_user, get_use
 from stocks.controllers.product_controller import create_product, remove_product, get_product
 from stocks.controllers.stock_controller import get_stock, set_stock, get_stock_overview
 from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+import threading
+
 
 app = Flask(__name__)
+
+
+# Auto-generate Redis reports 2 seconds after startup (to give enough time for the DB to start up as well). Once done for the first time, repeat it recursively every 60s to refresh the cache.
+def generate_reports_and_cache():
+    threading.Timer(2.0, get_report_highest_spending_users, args=(True,)).start()
+    threading.Timer(2.0, get_report_best_selling_products, args=(True,)).start()
+    threading.Timer(60.0, generate_reports_and_cache).start()
+
+# Start the first execution
+generate_reports_and_cache()
 
 @app.get('/health-check')
 def health():
